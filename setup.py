9f76a06c9f08d86895b7aa70127dc344d9a7406e
lucemia <lucemia@gmail.com>
2014-06-08 03:39:57 +0000

    link to r3 in submodule

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup, Extension
+
 setup(name="pyr3", version="1.0",
     ext_modules=[Extension(
     "pyr3",
     ["pyr3.c"],
-    libraries=["pcre","r3"],
+    libraries=["pcre"],
     include_dirs=['./r3/include', './r3', '/opt/local/include'],
-    library_dirs=['/usr/local/lib', '/opt/local/lib'],
-    runtime_library_dirs=['/usr/local/lib']
+    library_dirs=['./r3', '/usr/local/lib', '/opt/local/lib'],
+    runtime_library_dirs=['./r3', '/usr/local/lib']
 )])
 
 try:
