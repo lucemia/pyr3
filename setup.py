3fc578ef59803ce18528022c5fa879f90cadcd16
David Chen <lucemia@gmail.com>
2014-06-09 11:20:14 +0800

    update setup.py to fix #5

--- setup.py
+++ setup.py
@@ -1,19 +1,47 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
 
 import os
 fs = ['./r3/src/%s'%k for k in os.listdir('./r3/src') if k.endswith('.c') and 'gvc' not in k and 'json' not in k]
 
+cmdclass = {}
+ext_modules = []
+
+if False and use_cython:
+    ext_modules += [
+        Extension(
+            "pyr3",
+            ["cpyr3.pyx"] + fs + ['./r3/3rdparty/zmalloc.c'],
+            libraries=["pcre"],
+            include_dirs=['./r3/include', './r3', '/opt/local/include', './r3/3rdparty'],
+            library_dirs=['/usr/local/lib', '/opt/local/lib'],
+            extra_compile_args=['-std=c99'],
+            extra_link_args=['-std=c99']
+        )
+    ]
+    cmdclass.update({ 'build_ext': build_ext })
+else:
+    ext_modules += [
+        Extension(
+            "pyr3",
+            ["cpyr3.c"] + fs + ['./r3/3rdparty/zmalloc.c'],
+            libraries=["pcre"],
+            include_dirs=['./r3/include', './r3', '/opt/local/include', './r3/3rdparty'],
+            library_dirs=['/usr/local/lib', '/opt/local/lib'],
+            extra_compile_args=['-std=c99'],
+            extra_link_args=['-std=c99']
+        )
+    ]
+
+
 setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension(
-        "pyr3",
-        ["cpyr3.pyx"] + fs + ['./r3/3rdparty/zmalloc.c'],
-        libraries=["pcre"],
-        include_dirs=['./r3/include', './r3', '/opt/local/include', './r3/3rdparty'],
-        library_dirs=['/usr/local/lib', '/opt/local/lib'],
-        extra_compile_args=['-std=c99'],
-        extra_link_args=['-std=c99']
-    )]
+    cmdclass = cmdclass,
+    ext_modules = ext_modules
 )
